3225119281a7bb70f77701eb548e93a4a355b5a0
ZacharyWeiner <zach@cloud.com>
2017-06-27 12:45:15 -0500

    updated seeds for company user relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Company.create!(name: "CocaCola", website: 'www.coke.com')
-Company.create!(name: "Pepsi", website: 'www.pepsi.com')
+coke = Company.create!(name: "CocaCola", website: 'www.coke.com')
+ccUser = User.create!(email: 'coke@coke.com', password: 'password', password_confirmation: 'password')
+coke.users << ccUser 
+
+pepsi = Company.create!(name: "Pepsi", website: 'www.pepsi.com')
+pUser = User.create!(email: 'pepsi@pepsi.com', password: 'password', password_confirmation: 'password')
+pepsi.users << pUser 
+
+tomford = Company.create!(name: "Tom Ford", website: 'www.tomford.com')
+tfUser = User.create!(email: 'tom-ford@tom-ford.com', password: 'password', password_confirmation: 'password')
+tomford.users << tfUser
 
 User.create!(email: 'user@nomcrea.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'company-user@nomcrea.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'company-user2@nomcrea.com', password: 'password', password_confirmation: 'password')
-User.create!(email: 'company-user3@nomcrea.com', password: 'password', password_confirmation: 'password')
+
+
+
+
 
