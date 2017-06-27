e30eb489cad56a3a55e0ff643a99e951a2da70aa
ZacharyWeiner <zach@cloud.com>
2017-06-27 11:59:57 -0500

    added companies scaffold

---
+++ app/views/companies/_company.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! company, :id, :name, :website, :logo, :users_id, :created_at, :updated_at
+json.url company_url(company, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :company_users
+  devise_for :users
+  resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 end
