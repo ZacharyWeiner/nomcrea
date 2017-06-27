cb15fad7b3a6df190b337cbce5c297c85fb2bd90
ZacharyWeiner <zach@cloud.com>
2017-06-27 17:23:06 -0500

    added equipment taggable to portfolio_item and attachments

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -1,3 +1,5 @@
 class PortfolioItem < ApplicationRecord
   belongs_to :portfolio
+  has_many :portfolio_item_attachments
+  acts_as_taggable_on :equipment
 end
--- app/models/portfolio_item_attachment.rb
+++ app/models/portfolio_item_attachment.rb
@@ -1,3 +1,4 @@
 class PortfolioItemAttachment < ApplicationRecord
   belongs_to :portfolio_item
+  acts_as_taggable_on :equipment
 end
